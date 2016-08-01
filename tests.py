024d436b4396af09d835bce88a0f5f10a631b523
emperical <k.slootweg@gmail.com>
2016-08-01 12:37:34 +0200

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_index(self):
         rv = self.app.get('/')
-        assert b'hello, world' in rv.data
+        assert b'hello, foo' in rv.data
 
 if __name__ == '__main__':
     unittest.main()
